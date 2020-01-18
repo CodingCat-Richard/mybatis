642b679b5bb3eaddd1b333460f6a39d22c3f59a6
CodingCat-Richard <2462822181@qq.com>
2020-01-18 13:16:44 +0800

    second

--- src/main/java/com/xiao/dao/IDao.java
+++ src/main/java/com/xiao/dao/IDao.java
@@ -2,6 +2,8 @@ package com.xiao.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Select;
+
 import com.xiao.domin.User;
 
 public interface IDao {
@@ -9,5 +11,6 @@ public interface IDao {
 	 * 查找所有记录
 	 * @return
 	 */
+	//@Select("select * from user")
 	 List<User> findAll();
 }
